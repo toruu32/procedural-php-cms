732f76d4c4e14e801e949f02ee1c0d976d55a4de
toruu32 <toruu32@gmail.com>
2024-01-01 14:02:10 +0200

    add cat

---
+++ app/admin/categories.php
@@ -0,0 +1,54 @@
+<?php include_once('./templates/head.php'); ?>
+<?php include_once('../functions/functions.php'); ?>
+<div id="wrapper">
+
+    <?php include_once('./templates/navigation.php'); ?>
+
+    <div id="page-wrapper bg-dark">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-6">
+                    <h1 class="" style="color: #777;">
+                        Add Category
+                    </h1>
+                    <form action="" role="form" method="post">
+                        <div class="form-group">
+                            <label for="addCategory" style="color: #777;">Category Name</label>
+                            <input type="text" class="form-control" id="addCategory" name="categoryName" placeholder="Enter category name">
+                        </div>
+                        <?php addCategory(); ?>
+                        <button type="submit" name="submit" class="btn btn-primary">Add Category</button>
+                    </form>
+                </div>  
+                <div class="col-lg-6">
+                    <h1 class="" style="color: #777;">
+                        Categories List
+                    </h1>
+                    <table class="table">
+                        <thead class="text-primary">
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-warning">
+                            <?php categoriesList(); ?>
+                        </tbody>
+                    </table>
+                </div>  
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php include_once('../templates/footer.php') ?>
--- app/functions/functions.php
+++ app/functions/functions.php
@@ -53,7 +53,7 @@ function renderPosts()
     echo !empty($postTemplate) ? $postTemplate : 'No posts yet.';
 
     mysqli_stmt_close($stmt);
-}
+};
 
 // GET POSTS TEMPLATE
 function getPostsTemplate($row)
@@ -80,6 +80,56 @@ function getPostsTemplate($row)
         <hr>
         <p>$postContent</p>
         <a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>";
-}
+};
+
+// PRINT CATEGORIES LIST IN CATEGORIES.PHP
+function categoriesList()
+{
+    global $connection;
+    $query = "SELECT CATEGORY_ID, CATEGORY_NAME FROM CATEGORIES";
+    if ($stmt = mysqli_prepare($connection, $query)) {
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $categoryId = intval($row["CATEGORY_ID"]);
+            $categoryName = htmlspecialchars($row["CATEGORY_NAME"]);
+            echo '<tr>';
+            echo "<td>$categoryId</td>";
+            echo "<td>$categoryName</td>";
+            echo '<tr>';
+        }
+    } else {
+        die('Statement preparation failed: ' . mysqli_error($connection));
+    }
+    mysqli_free_result($result);
+    mysqli_stmt_close($stmt);
+};
+
+function addCategory()
+{
+    global $connection;
+
+    if (isset($_POST['submit'])) {
+
+        $categoryName = mysqli_escape_string($connection, $_POST['categoryName']);
+
+        if (!empty($categoryName)) {
+
+            $query = "INSERT INTO CATEGORIES (CATEGORY_NAME) VALUES ('$categoryName')";
+
+            if ($stmt = mysqli_prepare($connection, $query)) {
+
+                mysqli_stmt_execute($stmt);
+
+                mysqli_stmt_close($stmt);
+            } else {
+                die('Error' . mysqli_error($connection));
+            }
+        } else {
+            echo 'Empty input';
+        }
+    }
+};
+
 
 ?>
